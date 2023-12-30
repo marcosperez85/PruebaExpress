046ed03847bf39bb5c861044970a8c190b18e67b
Marcos <perezmarcos85@gmail.com>
2023-12-30 12:30:45 -0300

    Defino ruta para los request y agrego ejemplo de middleware

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const app = express();
 const port = 3000; // You can choose any port number
 
 // Define routes and middleware here
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Define Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Example middleware to log requests
+app.use((req, res, next) => {
+    console.log(`Request received at ${new Date()}`);
+    next();
+});
+
+// Handle Static Files
+app.use(express.static('public'));
